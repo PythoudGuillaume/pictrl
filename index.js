77e87f76ac0a4924a64336c3e1fb7fb3aba0c1d0
gravelpyt <pythoudg@gmail.com>
2017-06-22 15:08:34 +0200

    test atom git integration

--- index.js
+++ index.js
@@ -1,13 +1,10 @@
-var http = require("http"); 
+var http = require("http");
 
 var port = 3000;
 
-http.createServer(function(reqst, resp) { 
-    resp.writeHead(200, {'Content-Type': 'text/plain'}); 
-    resp.end('PICTL'); 
-}).listen(port); 
+http.createServer(function(reqst, resp) {
+    resp.writeHead(200, {'Content-Type': 'text/plain'});
+    resp.end('PICTL');
+}).listen(port);
 
-console.log('pictl is running');
-
-
-
+console.log('lolilol');
